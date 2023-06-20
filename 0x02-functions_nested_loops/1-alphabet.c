e790a714f82be6c9579b98b8cccafb3e972a9658
Aderhm <lahyani.ad@gmail.com>
2023-06-20 12:51:57 +0100

    Fix betty issues

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Prints alphabet in lowercase.
  *
- * Prints alphabet in lowercase
- * Return: void
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  **/
 void print_alphabet(void)
 {
