500dfe9512b3283abbbb8f05f9c93d239baa1878
aderhm <lahyani.ad@gmail.com>
2023-06-20 12:32:58 +0100

    Fix betty issues

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,16 +10,16 @@
  **/
 int main(void)
 {
-    unsigned int i = 0;
-    char str[] = "_putchar";
+	unsigned int i = 0;
+	char str[] = "_putchar";
 
-    while (i < strlen(str))
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (i < strlen(str))
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
