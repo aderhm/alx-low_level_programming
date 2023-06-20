915124ffc2d329e5dffb2e1f8a442a0ebb63dc10
Aderhm <lahyani.ad@gmail.com>
2023-06-20 11:52:36 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
+#include "main.h"
 #include <unistd.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -8,6 +10,16 @@
  **/
 int main(void)
 {
-    write(1, "_putchar\n", 8);
+    unsigned int i = 0;
+    char str[] = "_putchar";
+    
+    while (i < strlen(str))
+    {
+        _putchar(str[i]);
+        i++;
+    }
+
+    _putchar('\n');
+    
     return (0);
 }
