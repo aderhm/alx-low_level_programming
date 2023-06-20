1591b2ff2071bdf9c93f1935be6008c1463621fe
Aderhm <lahyani.ad@gmail.com>
2023-06-20 10:11:22 +0100

    Trying the other way - !works

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,23 +9,48 @@
  **/
 int main(void)
 {
-	int i, j;
+	// int i, j;
 
-	for (i = 0; i <= 98; i++)
+	// for (i = 0; i <= 98; i++)
+	// {
+	// 	for (j = i + 1; j <= 99; j++)
+	// 	{
+	// 		putchar((i / 10) + '0');
+	// 		putchar((i % 10) + '0');
+	// 		putchar(' ');
+	// 		putchar((j / 10) + '0');
+	// 		putchar((j % 10) + '0');
+
+	// 		if (i == 98 && j == 99)
+	// 			continue;
+
+	// 		putchar(',');
+	// 		putchar(' ');
+	// 	}
+	// }
+
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-
-			if (i == 98 && j == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = k + 1; l <= 9; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i != 9 && j != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
 
