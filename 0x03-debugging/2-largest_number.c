c46dafb11382e113b275fd5951404c4c50def579
Aderhm <lahyani.ad@gmail.com>
2023-06-21 17:13:41 +0100

    Update func largest_numb

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,13 +13,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
+	{
 		largest = a;
-
-	if (b > a && b > c)
+	}
+	else if (b > a && b > c)
+	{
 		largest = b;
-
-	if (c > a && c > b)
+	}
+	else if (c > a && c > b)
+	{
 		largest = c;
+	}
 
 	return (largest);
 }
