8d8a6caf031d64df4e7b2ea78b811d9cf1bb8073
Aderhm <lahyani.ad@gmail.com>
2023-06-21 01:35:01 +0100

    Prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,19 @@
 int main(void)
 {
 	int i = 0;
-	long int a = 1, b = 2, fib;
+	unsigned long int a = 1, b = 2, fib;
 
-	printf("%li, ", a);
-	printf("%li, ", b);
+	printf("%lu, ", a);
+	printf("%lu, ", b);
 
 	while (i < 96)
 	{
 		fib = a + b;
-		printf("%li", fib);
+		printf("%lu", fib);
 		a = b;
 		b = fib;
 
-		if (i != 47)
+		if (i != 95)
 			printf(", ");
 
 		i++;
