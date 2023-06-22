a2d6d57eaa5e68f861dd200777eb1c6283a5ac43
Aderhm <lahyani.ad@gmail.com>
2023-06-22 15:40:49 +0100

    Fix the extra use of _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + 48);
-			}
-			else
-			{
-				_putchar((j / 10) + 48);
-				_putchar((j % 10) + 48);
-			}
+			if (j > 9)
+				_putchar(49);
+
+			_putchar((j % 10) + 48);
 		}
 
 		_putchar('\n');
