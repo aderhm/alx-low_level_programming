939451f14acf90eee86d5ee2aa29eb2b1ee65c7b
aderhm <lahyani.ad@gmail.com>
2023-06-29 09:25:11 +0100

    Betty fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,30 +9,30 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, j, k, destlen = 0, strlen = 0;
+	int i, j, k, destlen = 0, strlen = 0;
 
-    i = 0;
-    while (src[i] != '\0')
-    {
-       strlen++;
-       i++;
-    }
+	i = 0;
+	while (src[i] != '\0')
+	{
+		strlen++;
+		i++;
+	}
 
-    i = 0;
-    while (dest[i] != '\0')
-    {
-        destlen++;
-        i++;
-    }
-    
-    j = i + strlen;
-    k = 0;
-    while (i <= j)
-    {
-        dest[i] = src[k];
-        i++;
-        k++;
-    }
-    
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		destlen++;
+		i++;
+	}
+
+	j = i + strlen;
+	k = 0;
+	while (i <= j)
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+
 	return (dest);
 }
