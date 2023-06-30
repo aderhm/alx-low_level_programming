a108be40d7495d4dbda8a7f2d96892842cc17ee0
aderhm <lahyani.ad@gmail.com>
2023-06-30 13:29:19 +0100

    Cap words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: The string
+ *
+ * Return: The string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '.' || s[i] == '\n' || s[i] == '\t')
+		{
+			if (s[i] == '\t')
+				s[i] = ' ';
+
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] -= 32;
+		}
+
+		i++;
+	}
+
+	return (s);
+}
