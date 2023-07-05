32cb9e57213dfc8858d0fc008ef8156e6276eaf0
aderhm <lahyani.ad@gmail.com>
2023-07-05 18:31:57 +0100

    Prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-	while (*s)
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(s[0]);
-		s++;
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
