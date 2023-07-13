e938ed23775f225fffc8dad8030dcbb4a1c47e17
aderhm <lahyani.ad@gmail.com>
2023-07-13 19:17:53 +0100

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,16 +13,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *arr;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
 	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
 
-	i = 0;
-	while (*arr)
-		arr[i++] = 0;
+	i = size * nmemb;
+	while (i--)
+		arr[i] = 0;
 
 	return (arr);
 }
