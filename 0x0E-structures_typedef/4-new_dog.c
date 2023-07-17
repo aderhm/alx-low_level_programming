98e5afba730472e97e4249b85b2ba57749537cf9
aderhm <lahyani.ad@gmail.com>
2023-07-17 23:06:52 +0100

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	d = malloc(sizeof(*d));
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
 
