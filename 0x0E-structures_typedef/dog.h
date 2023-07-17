124e91ae428fe1b3fbf88d07f7980dacd81ac184
aderhm <lahyani.ad@gmail.com>
2023-07-17 21:51:26 +0100

    Betties

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - represents a dog and contains three members:
+* @name: A pointer to a character array that stores the name of the dog.
+* @age: Represents the age of the dog.
+* @owner: A pointer to a character array that stores the name of the owner.
+*/
 typedef struct dog
 {
 	char *name;
