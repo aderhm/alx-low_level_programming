72ead94b957d5ad3a7bbbdfdd497e0d0a2815469
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-07-19 22:32:51 +0100

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name using a function ptr.
+ * @name: a name,
+ * @f: a pointer to a function.
+ *
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
