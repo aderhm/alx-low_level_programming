10b657dae89c5e9fcdeba32a865aa21fce4b3b0c
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-07-27 23:04:18 +0100

    Free lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: a list
+ *
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
