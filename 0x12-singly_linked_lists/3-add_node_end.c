722e29bae6d8008ac1a10a3f82c58a802958c425
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-07-27 20:35:11 +0100

    Insert new node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list.
+ * @head: a list
+ * @str: string to insert in the new node.
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *last;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if ((*head) == NULL)
+	{
+		(*head) = new;
+		return (new);
+	}
+
+	last = *head;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	last->next = new;
+
+	return (last);
+}
