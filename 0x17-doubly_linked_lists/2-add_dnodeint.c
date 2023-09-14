4febf0e77c5e51ee46bb75df5450dd333817dc42
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-09-14 21:20:18 +0100

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,9 +17,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
         new->prev = NULL;
 	new->n = n;
-        new->next = NULL;
-	new->next = (*head);
-        (*head)->prev = new;
+
+        if ((*head) == NULL)
+                new->next = NULL;
+        else
+        {
+                new->next = *head;
+                (*head)->prev = new;
+        }
 
 	(*head) = new;
 
