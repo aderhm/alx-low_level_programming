723c4a7c29ef902c8d3531eea101c79a072fe5f8
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-08 10:00:53 +0100

    Get an element

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -19,10 +19,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((unsigned char *)key, ht->size);
 	item = ht->array[index];
 
-	if (item)
+	while (item)
 	{
 		if (strcmp(item->key, key) == 0)
 			return (item->value);
+		item = item->next;
 	}
 
 	return (NULL);
