f71567b49ec1fd13dd36f76dba25b4398d5dc7e8
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-08 09:58:27 +0100

    Get an element

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *item;
 
-	if (!ht || !key)
+	if (!ht || !key || !(*key))
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
