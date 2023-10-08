e78f0033b73c97ddbb5d6fca7829c1f123618585
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-08 07:27:11 +0100

    Betty

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -26,3 +29,5 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+#endif /* HASH_TABLES_H */
