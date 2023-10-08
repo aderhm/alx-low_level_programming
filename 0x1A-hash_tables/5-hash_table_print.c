7901b1871515b7ce56c27e987cec0b9881f072fb
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-10-08 10:19:30 +0100

    Print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - rints a hash table.
+ * @ht: the hash table.
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *item;
+	unsigned long int i;
+	char *separator;
+
+	separator = "";
+
+	printf("{");
+
+	i = 0;
+	while (i < ht->size)
+	{
+		item = ht->array[i];
+		while (item)
+		{
+			printf("%s'%s': '%s'", separator, item->key, item->value);
+			separator = ", ";
+			item = item->next;
+		}
+		i++;
+	}
+
+	printf("}\n");
+
+}
