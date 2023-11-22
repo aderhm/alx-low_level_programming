671221035ac806eccdce135b9d982bf30a09e0c6
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-11-22 23:14:54 +0100

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module calculates the perimeter of an island in a grid."""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island in grid."""
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                if i <= 0 or not grid[i - 1][j]:
+                    perimeter += 1
+                if i >= len(grid) - 1 or not grid[i + 1][j]:
+                    perimeter += 1
+                if j <= 0 or not grid[i][j - 1]:
+                    perimeter += 1
+                if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+                    perimeter += 1
+
+    return perimeter
